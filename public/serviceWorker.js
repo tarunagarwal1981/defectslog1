a6973426e22d27a8f89f3865e67d59fb59266de3
Tarun Agarwal <49944511+tarunagarwal1981@users.noreply.github.com>
2024-11-19 09:17:18 +0530

    Create serviceWorker.js

---
+++ public/serviceWorker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'defect-manager-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/css/main.css',
+  '/static/js/main.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
