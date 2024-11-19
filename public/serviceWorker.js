6b8df870c9c0360195e491935dde201c9965747a
Tarun Agarwal <49944511+tarunagarwal1981@users.noreply.github.com>
2024-11-19 10:16:52 +0530

    Update serviceWorker.js

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,19 +1,20 @@
 const CACHE_NAME = 'defect-manager-v1';
+
+// Add any URLs you want to cache
 const urlsToCache = [
   '/',
   '/index.html',
   '/static/css/main.css',
   '/static/js/main.js',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/favicon.ico',
+  '/logo192.png',
+  '/logo512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -23,10 +24,32 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
+        // Cache hit - return response
         if (response) {
           return response;
         }
-        return fetch(event.request);
+
+        // Clone the request
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          (response) => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
       })
-  );
+    );
 });
